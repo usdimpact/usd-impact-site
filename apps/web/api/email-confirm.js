ebf055db2c0d80d43c9cc804136abfebf776f23c
usdimpact <MIRCEA.MANAGEMENT@GMAIL.COM>
2026-09-11 23:58:31 +0300

    fix(email): remove standalone confirm function

--- apps/web/api/email-confirm.js
+++
@@ -1,5 +0,0 @@
-import { handleMarketingOptInConfirmation } from '../src/lib/marketing-opt-in-handler.js';
-
-export default async function handler(request, response) {
-  return handleMarketingOptInConfirmation(request, response);
-}

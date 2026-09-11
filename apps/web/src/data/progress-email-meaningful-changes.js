98a86ab371827e886e0700c17a63a117d2a52180
usdimpact <MIRCEA.MANAGEMENT@GMAIL.COM>
2026-09-12 01:16:55 +0300

    feat(email): add governed progress meaningful-change registry

---
+++ apps/web/src/data/progress-email-meaningful-changes.js
@@ -0,0 +1,11 @@
+export const PROGRESS_EMAIL_CHANGE_REGISTRY_VERSION = 1;
+
+export const progressEmailMeaningfulChangeRegistry = Object.freeze([
+  Object.freeze({
+    changeId: 'weekly-report:2026-09-04',
+    sourceType: 'weekly_report',
+    sourceId: '2026-09-04',
+    priority: 'P2',
+    occurredAt: '2026-09-07T14:13:55.977Z',
+  }),
+]);

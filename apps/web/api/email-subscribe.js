2563920178a82228fa0e6cd4727f0c3f77bc9932
usdimpact <MIRCEA.MANAGEMENT@GMAIL.COM>
2026-09-11 23:59:56 +0300

    fix(email): remove standalone subscribe function

--- apps/web/api/email-subscribe.js
+++
@@ -1,5 +0,0 @@
-import { handleMarketingOptInRequest } from '../src/lib/marketing-opt-in-handler.js';
-
-export default async function handler(request, response) {
-  return handleMarketingOptInRequest(request, response);
-}
